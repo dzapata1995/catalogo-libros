ab82b27c10ad148d8ea60c8893642a0ff3a36153
Diego Zapata <diego.zapata1995@hotmail.com>
2024-12-06 15:28:24 -0300

    (feat) Inicio de proyecto.

---
+++ src/main/java/com/dzapata/literatura/ChallegeLiteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.dzapata.literatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallegeLiteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallegeLiteraturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dzapata/literatura/ChallegeLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzapata.literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallegeLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
