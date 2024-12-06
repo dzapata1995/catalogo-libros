c631e77ca7aacc9dd92fbf2a3d7107f8467fdacf
Diego Zapata <diego.zapata1995@hotmail.com>
2024-12-06 15:33:01 -0300

    (fix) correcciones ortograficas

--- src/main/java/com/dzapata/literatura/ChallengeLiteraturaApplication.java
+++ src/main/java/com/dzapata/literatura/ChallengeLiteraturaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallegeLiteraturaApplication {
+public class ChallengeLiteraturaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChallegeLiteraturaApplication.class, args);
+		SpringApplication.run(ChallengeLiteraturaApplication.class, args);
 	}
 
 }
